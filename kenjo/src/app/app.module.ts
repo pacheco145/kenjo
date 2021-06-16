7970b5433d5d6f27ba8f79d261fa1ec0dbc60871
pacheco145 <pablopr145@hotmail.es>
2021-06-17 00:10:20 +0200

    FEAT: integrated add artist and add album in app

--- kenjo/src/app/app.module.ts
+++ kenjo/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddAlbumComponent } from './pages/add-album/add-album.component';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AddArtistComponent } from './pages/add-artist/add-artist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddAlbumComponent,
+    AddArtistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
