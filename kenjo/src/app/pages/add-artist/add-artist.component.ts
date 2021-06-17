42fae701e7b4514a8cd0b3a94da398c5a26a7a89
pacheco145 <pablopr145@hotmail.es>
2021-06-18 00:17:43 +0200

    FEAT: add artist page

--- kenjo/src/app/pages/add-artist/add-artist.component.ts
+++ kenjo/src/app/pages/add-artist/add-artist.component.ts
@@ -1,5 +1,5 @@
-import { PostArtistService } from './../../shared/services/postArtist/post-artist.service';
-import { PostAlbumService } from './../../shared/services/postAlbum/post-album.service';
+import { PostArtistService } from '../../shared/services/artists/postArtist/post-artist.service';
+import { PostAlbumService } from './../../shared/services/albums/postAlbum/post-album.service';
 import { FormBuilder } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,24 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddArtistComponent implements OnInit {
 
-  newArtist;
-  
-  constructor(public formBuilder: FormBuilder, private postArtist: PostArtistService) {
-    this.newArtist = this.formBuilder.group({
-      name: '',
-      photoUrl: 'https://los40.com/los40/imagenes/2019/10/18/los40classic/1571386693_641304_1571387117_miniatura_normal.jpg',
-      birthdate: '',
-      deathDate: null,
-    });
-  }
-  
+  constructor() { }
 
-  ngOnInit(): void {}
-
-
-  addArtist = () => {
-    console.log(this.newArtist.value)
-    this.postArtist.addArtist(this.newArtist.value).subscribe(res=>console.log(res))
+  ngOnInit(): void {
   }
 }
 
